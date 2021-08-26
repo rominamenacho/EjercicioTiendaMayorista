f9df97aaec5724507f9bcf2ab5db08c44666aa4d
rominamenacho <43242020+rominamenacho@users.noreply.github.com>
2021-08-26 16:31:13 -0300

    se limpiaron unas lineas redundantes

--- CotizadorTiendaMayorista/Model/Camisa.cs
+++ CotizadorTiendaMayorista/Model/Camisa.cs
@@ -41,28 +41,14 @@ namespace CotizadorTiendaMayorista.Modelo
         {
             decimal retorno = base.CalculatePrice();
             if (Manga is Corta ) {
-                if(Cuello is Mao)
-                {
-                    retorno = retorno - System.Math.Round(System.Convert.ToDecimal(retorno * 0.10M), 2);// rn1
 
-                    retorno = retorno + System.Math.Round(System.Convert.ToDecimal(retorno * 0.03M), 2); ; //rn2
-
-                 
-                }
-                else
-                {
-                    retorno = retorno - System.Math.Round(System.Convert.ToDecimal(retorno * 0.10M), 2);// rn1
-
-
-                }
-
-            }
-            else
-            {
-                if(Cuello is Mao)
+                retorno = retorno - System.Math.Round(System.Convert.ToDecimal(retorno * 0.10M), 2);// rn1
+               
+                if (Cuello is Mao)
                 {
                     retorno = retorno + System.Math.Round(System.Convert.ToDecimal(retorno * 0.03M), 2); ; //rn2
                 }
+                
             }
            
 
